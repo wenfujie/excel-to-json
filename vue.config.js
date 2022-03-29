3c1aef46f5350767fa1f0ef694e8c74a5cfdfa37
温富杰 <wenfujie@dianchu.com>
2022-03-29 14:52:13 +0800

    修改vue打包配置

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+/*
+ * @Date: 2022-03-29 14:50:55
+ * @LastEditors: wenfujie
+ * @LastEditTime: 2022-03-29 14:50:56
+ * @FilePath: /excel-to-json/vue.config.js
+ */
+
+module.exports = {
+  publicPath: '/excel-to-json/'
+}
